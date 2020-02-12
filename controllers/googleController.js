5e082e04a8b04925218f2a9f71689f2c5f73b132
Jacob Beuchert <jacobbeuchert@gmail.com>
2020-02-11 22:46:56 -0700

    Changing code to get the app to work again

--- controllers/googleController.js
+++ controllers/googleController.js
@@ -1,11 +1,6 @@
 const axios = require("axios");
 const db = require("../models");
 
-// Defining methods for the googleController
-
-// findAll searches the Google Books API and returns only the entries we haven't already saved
-
-// It also makes sure that the books returned from the API all contain a title, author, link, description, and image
 module.exports = {
   findAll: function(req, res) {
     const { query: params } = req;
@@ -32,6 +27,6 @@ module.exports = {
         )
       )
       .then(books => res.json(books))
-      .catch(err => res.status(400).json(err));
+      .catch(err => res.status(422).json(err));
   }
 };
